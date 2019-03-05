950026f85db6471ef5e385534823dc6591205f83
Agam Brahma <agam.brahma@gmail.com>
2019-03-05 00:30:59 -0800

    Playing around with timing of various operations

---
+++ state-play/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for state-play
+
+## Unreleased changes
---
+++ state-play/app/Main.hs
@@ -0,0 +1,57 @@
+module Main where
+
+import System.CPUTime
+import System.IO
+
+-- TODO: define a record for computations ...
+
+timing :: IO a -> IO a
+timing comp = do
+  start <- getCPUTime
+  v <- comp  -- Perform the actual computation
+  end <- getCPUTime
+  let diff = end - start  -- fyi: picoseconds
+  let diff_millies = (fromIntegral diff) / (10 ^ 9)
+  putStrLn $ "Time: " ++ (show diff_millies) ++ " msec."
+  pure v
+
+-- Simplest possible thing, as a baseline
+simpleComp :: IO ()
+simpleComp = do
+  putStrLn "Hello World"
+
+-- Open a file in /tmp, write a large string
+writeComp :: String -> IO ()
+writeComp s = do
+  writeFile "/tmp/foobar.txt" s
+  pure ()
+
+
+-- Simple, non-randomized string
+makeLongString :: Int -> String
+makeLongString count =
+  take count $ cycle ['a' .. 'z']
+
+main :: IO ()
+main = do
+  timing $ simpleComp
+
+  timing $ writeComp "something"
+
+  let str1 = makeLongString 5000
+  timing $ writeComp str1
+
+  let str2 = makeLongString 500000
+  timing $ writeComp str2
+
+
+--------------------------------------------------
+-- Sample run
+----------------------------------------
+
+-- > $ stack exec -- state-play-exe                                                                   
+-- Hello World
+-- Time: 0.309 msec.
+-- Time: 0.485 msec.
+-- Time: 0.585 msec.
+-- Time: 40.243 msec.
